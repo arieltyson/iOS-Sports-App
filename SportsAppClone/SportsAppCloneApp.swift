587d164679c8777bd1119ca23d8a60de3b604e55
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2025-12-03 01:05:50 -0800

    Initial Commit

---
+++ SportsAppClone/SportsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsAppCloneApp.swift
+//  SportsAppClone
+//
+//  Created by Ariel Tyson on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SportsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
